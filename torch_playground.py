63b34c9e0fd1d06ce2edb232f2addd14112915d6
Matt Megarry <mattmegarry@gmail.com>
2023-07-24 14:02:05 +0100

    some basic torch capabilities to learn

--- torch_playground.py
+++ torch_playground.py
@@ -1,5 +1,44 @@
 import torch
 
-# 3x3 tensor x filled with random values
-x = torch.rand(3, 3)
+# Create tensor 
+x = torch.tensor([1, 2, 3]) 
+
+# Indexing
+print(x[0]) 
+
+# Math operations
+print(x + 2)
+
+# Reshape 
+x = x.view(3, 1)
 print(x)
+
+# One
+torch.ones(3, 1)
+
+# Broadcasting
+print(x + torch.ones(3, 1)) 
+
+# Concatenate 
+y = torch.tensor([4, 5, 6]).view(3, 1)
+print(torch.cat([x, y], dim=1))
+
+# Linear algebra
+print(x.matmul(y.t())) 
+
+# Reduce
+print(x.sum())
+
+# GPU tensor
+if torch.cuda.is_available():
+  x = x.to('cuda') 
+
+# Gradient
+floating_tensor = torch.tensor([1, 2, 3], dtype=torch.float, requires_grad=True)
+floating_tensor.requires_grad_()
+floating_tensor.backward(torch.randn(3))
+print(floating_tensor.grad)
+
+# 3x3 tensor x filled with random values
+y = torch.rand(3, 3)
+print(y)
