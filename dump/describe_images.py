d760ff30514a36c2a59667edfceedef036bfc618
Gradient Gigglers <gradient@gradient.com>
2023-07-25 12:35:21 +0200

    inspecting tensor shapes

--- dump/describe_images.py
+++ dump/describe_images.py
@@ -1,13 +1,13 @@
 from torchvision import transforms
 
-images_dataset = FolderDataset("images", transform=transforms.ToTensor())
+full_dataset = FolderDataset("images", transform=transforms.ToTensor())
 image_to_show = 0
 
-print(f"\n\n{len(images_dataset)} images in the dataset")
-print(f"images_dataset is of type {type(images_dataset)}\n\n")
+print(f"\n\n{len(full_dataset)} images in the dataset")
+print(f"full_dataset is of type {type(full_dataset)}\n\n")
 
-print(f"Each element of the dataset is a {type(images_dataset[image_to_show])}")
-print(f"...with a length of {len(images_dataset[image_to_show])}\n\n")
+print(f"Each element of the dataset is a {type(full_dataset[image_to_show])}")
+print(f"...with a length of {len(full_dataset[image_to_show])}\n\n")
 
-print(f"Each element of the tuple is a {type(images_dataset[image_to_show][0])}\n\n")
-print(f"The tensor values are (themselves vectors, tensors??): [channels, vertical_pixels, horizontal_pixels]: {images_dataset[image_to_show][0].shape}\n\n")
+print(f"Each element of the tuple is a {type(full_dataset[image_to_show][0])}\n\n")
+print(f"The tensor values are (themselves vectors, tensors??): [channels, vertical_pixels, horizontal_pixels]: {full_dataset[image_to_show][0].shape}\n\n")
--- tutorial-copy/image_similarity_train.py
+++ tutorial-copy/image_similarity_train.py
@@ -17,6 +17,27 @@ transforms = T.Compose([T.ToTensor()]) # Normalize the pixels and convert to ten
 
 full_dataset = FolderDataset("../images/", transforms) # Create folder dataset.
 
+image_to_show = 0
+
+print(f"\n\n{len(full_dataset)} images in the dataset")
+print(f"full_dataset is of type {type(full_dataset)}\n\n")
+
+print(f"Each element of the dataset is a {type(full_dataset[image_to_show])}")
+print(f"...with a length of {len(full_dataset[image_to_show])}\n\n")
+
+print(f"Each element of the tuple is a {type(full_dataset[image_to_show][0])}\n\n")
+print(f"The tensor values: [channels, vertical_pixels, horizontal_pixels]: {full_dataset[image_to_show][0].shape}\n\n")
+tensor = full_dataset[image_to_show][0]
+print(tensor)
+print(f"channels: {tensor.shape[0]}")
+print(f"vertical_pixels: {tensor.shape[1]}")
+print(f"horizontal_pixels: {tensor.shape[2]}")      
+
+
+# make sure random order
+
+# exit()
+
 train_size = 0.75
 val_size = 1 - train_size
 
